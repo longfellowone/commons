accb0de7fbeb737650171829eef58b57b0569957
Matt <matt@home.localdomain>
2018-11-12 16:24:52 -0800

    Update

--- n1/5.go
+++ n1/5.go
@@ -5,16 +5,16 @@ import "fmt"
 type hotdog int
 
 var x hotdog
-var y hotdog
+var y int
 
 func main() {
 	fmt.Println(x)
 	fmt.Printf("%T\n", x)
 	x = 42
 	fmt.Println(x)
+	fmt.Printf("%T\n", x)
 
-	y = hotdog(x)
+	y = int(x)
 	fmt.Println(y)
 	fmt.Printf("%T\n", y)
-
 }
---
+++ n3/1.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	//for i := 1; i <= 10; i++ {
+	//	for i := 1; i <= 2; i++ {
+	//		fmt.Println(i)
+	//	}
+	//}
+
+}
